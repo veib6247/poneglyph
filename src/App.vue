a17ee1d266cf829eba6f36337a1500e3103c71ce
Bryan Olandres <ozoneblacklight@outlook.com>
2023-01-01 15:30:26 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,269 @@
+<script setup>
+  // components
+  import VdisplayOption from './components/VdisplayOption.vue'
+
+  // utils
+  import { ref } from 'vue'
+
+  const numberCode = ref('')
+
+  const services = ref({
+    nhentai: true,
+    tsumino: false,
+    hitomila: false,
+    ehentai: false,
+    danbooru: false,
+    gelbooru: false,
+    sankaku: false,
+    pixiv: false,
+    yandere: false,
+    e621: false,
+    rule34: false,
+    getchu: false,
+  })
+</script>
+
+<template>
+  <div class="hero min-h-screen bg-base-100">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Poneglyph</h1>
+        <div class="py-6">
+          <input
+            type="text"
+            placeholder="e.g. 177013"
+            class="input input-bordered w-full max-w-xs"
+            v-model="numberCode" />
+
+          <!-- checkboxes for the services -->
+          <div class="flex flex-col">
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">NHentai</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.nhentai"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Tsumino</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.tsumino"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Hitomi.la</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.hitomila"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">e-hentai</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.ehentai"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Danbooru</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.danbooru"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Gelbooru</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.gelbooru"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Sankaku</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.sankaku"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Pixiv</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.pixiv"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Yande.re</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.yandere"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">e621</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.e621"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Rule 34</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.rule34"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+
+            <!--  -->
+            <div class="form-control">
+              <label class="label cursor-pointer">
+                <span class="label-text">Getchu</span>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  v-model="services.getchu"
+                  :true-value="true"
+                  :false-value="false" />
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- display components here -->
+  <section class="bg-base-100">
+    <div class="container mx-auto p-3 pb-6">
+      <div class="flex flex-col gap-3">
+        <VdisplayOption
+          link-title="NHentai"
+          :hyper-link="`https://nhentai.net/g/${numberCode}`"
+          v-if="services.nhentai && numberCode" />
+
+        <VdisplayOption
+          link-title="Tsumino"
+          :hyper-link="`https://www.tsumino.com/entry/${numberCode}`"
+          v-if="services.tsumino && numberCode" />
+
+        <VdisplayOption
+          link-title="Hitomi.la"
+          :hyper-link="`https://hitomi.la/reader/${numberCode}.html`"
+          v-if="services.hitomila && numberCode" />
+
+        <VdisplayOption
+          link-title="e-hentai"
+          :hyper-link="`https://e-hentai.org/g/${numberCode}`"
+          v-if="services.ehentai && numberCode" />
+
+        <VdisplayOption
+          link-title="Danbooru"
+          :hyper-link="`https://danbooru.donmai.us/posts/${numberCode}`"
+          v-if="services.danbooru && numberCode" />
+
+        <VdisplayOption
+          link-title="Gelbooru"
+          :hyper-link="`https://gelbooru.com/index.php?page=post&s=view&id=${numberCode}`"
+          v-if="services.gelbooru && numberCode" />
+
+        <VdisplayOption
+          link-title="Sankaku"
+          :hyper-link="`https://chan.sankakucomplex.com/post/show/${numberCode}`"
+          v-if="services.sankaku && numberCode" />
+
+        <VdisplayOption
+          link-title="Pixiv"
+          :hyper-link="`https://www.pixiv.net/en/artworks/${numberCode}`"
+          v-if="services.pixiv && numberCode" />
+
+        <VdisplayOption
+          link-title="Yande.re"
+          :hyper-link="`https://yande.re/post/show/${numberCode}`"
+          v-if="services.yandere && numberCode" />
+
+        <VdisplayOption
+          link-title="e621"
+          :hyper-link="`https://e621.net/post/show/${numberCode}`"
+          v-if="services.e621 && numberCode" />
+
+        <VdisplayOption
+          link-title="rule34"
+          :hyper-link="`https://rule34.xxx/index.php?page=post&s=view&id=${numberCode}`"
+          v-if="services.rule34 && numberCode" />
+
+        <VdisplayOption
+          link-title="Getchu"
+          :hyper-link="`http://www.getchu.com/soft.phtml?id=${numberCode}`"
+          v-if="services.getchu && numberCode" />
+      </div>
+    </div>
+  </section>
+
+  <footer class="footer p-10 bg-base-200 text-neutral-content"></footer>
+</template>
---
+++ src/components/VdisplayOption.vue
@@ -0,0 +1,58 @@
+<script setup>
+  //
+  const props = defineProps({
+    linkTitle: {
+      type: String,
+      required: true,
+    },
+
+    hyperLink: {
+      type: String,
+      required: true,
+    },
+  })
+
+  /**
+   *
+   */
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(props.hyperLink)
+
+    console.info(`"${props.hyperLink}" has been copied to the clipboard`)
+  }
+</script>
+
+<template>
+  <div class="flex gap-2 place-content-center">
+    <a :href="hyperLink" class="btn w-60 gap-2 glass" target="_blank">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+      </svg>
+      {{ linkTitle }}
+    </a>
+
+    <button class="btn glass btn-square" @click="copyToClipboard">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+      </svg>
+    </button>
+  </div>
+</template>
